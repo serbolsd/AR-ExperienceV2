54b189d08291c07d3ce73ea3ed5007ce1e5bd263
serbolsd <32022302+serbolsd@users.noreply.github.com>
2020-10-27 11:31:51 -0700

    Combine the changeProperty scrip with the sliders

--- AR-Experience/Assets/Scripts/lpa/LPAChangePropertys.cs
+++ AR-Experience/Assets/Scripts/lpa/LPAChangePropertys.cs
@@ -14,6 +14,8 @@ public class LPAChangePropertys : MonoBehaviour
   public FloatParameter m_contraste;
   public FloatParameter m_Distorcion;
 
+  public lpaUI m_UI;
+
   const float m_minRange1 = 4.5f;
   const float m_maxRange1 = 5.0f;
   const float m_minRange2 = 6.7f;
@@ -35,6 +37,8 @@ public class LPAChangePropertys : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
+    m_UI = FindObjectOfType<lpaUI>();
+
     cube = Random.Range(0,2);
     m_camLensDistorcion=m_camPostProcess.GetSettings<LensDistortion>();
     m_camDepthOfField = m_camPostProcess.GetSettings<DepthOfField>();
@@ -44,6 +48,15 @@ public class LPAChangePropertys : MonoBehaviour
     m_saturation.value = 0.0f;
     m_contraste.value = 0.0f;
     m_Distorcion.value = 0.0f;
+
+    m_UI.SetSliderColor(Color.red, lpaUI.SLIDERS.kFocalLenght);
+    m_UI.SetSliderColor(Color.red, lpaUI.SLIDERS.kSaturation);
+    m_UI.SetSliderColor(Color.red, lpaUI.SLIDERS.kContrast);
+
+    m_UI.SetSliderValue(lpaUI.SLIDERS.kFocalLenght,m_focusDistance.value);
+    m_UI.SetSliderValue(lpaUI.SLIDERS.kSaturation, m_saturation.value);
+    m_UI.SetSliderValue(lpaUI.SLIDERS.kContrast,m_contraste.value);
+
     m_ObjetiveSaturation = Random.Range(-100,100);
     m_maxSaturation = m_ObjetiveSaturation + 10;
     if (m_maxSaturation>100)
@@ -56,9 +69,9 @@ public class LPAChangePropertys : MonoBehaviour
     m_maxContrast = m_ObjetiveContrast + 10;
     if (m_maxContrast > 100)
       m_maxContrast = 100;
-    m_maxContrast = m_ObjetiveContrast - 10;
-    if (m_maxContrast < -50)
-      m_maxContrast = -50;
+    m_minContrast = m_ObjetiveContrast - 10;
+    if (m_minContrast < -50)
+      m_minContrast = -50;
 
     m_ObjetiveDistorcion = Random.Range(-30,30);
     m_maxDistorcion = m_ObjetiveDistorcion + 5;
@@ -72,26 +85,42 @@ public class LPAChangePropertys : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    m_focusDistance.value = m_UI.GetSliderValue(lpaUI.SLIDERS.kFocalLenght);
+    m_saturation.value = m_UI.GetSliderValue(lpaUI.SLIDERS.kSaturation);
+    m_contraste.value = m_UI.GetSliderValue(lpaUI.SLIDERS.kContrast);
+
     m_camDepthOfField.focusDistance = m_focusDistance;
     m_camColorGrading.saturation = m_saturation;
     m_camColorGrading.contrast = m_contraste;
     m_camLensDistorcion.intensity = m_Distorcion;
     if (checkFocusDistance())
     {
-
+      m_UI.SetSliderColor(Color.green, lpaUI.SLIDERS.kFocalLenght);
+    }
+    else
+    {
+      m_UI.SetSliderColor(Color.red, lpaUI.SLIDERS.kFocalLenght);
     }
     if (checkSaturation())
     {
-
+      m_UI.SetSliderColor(Color.green, lpaUI.SLIDERS.kSaturation);
+    }
+    else
+    {
+      m_UI.SetSliderColor(Color.red, lpaUI.SLIDERS.kSaturation);
     }
     if (checkContrast())
     {
-
+      m_UI.SetSliderColor(Color.green, lpaUI.SLIDERS.kContrast);
     }
-    if (checkDistorcion())
+    else
     {
-
+      m_UI.SetSliderColor(Color.red, lpaUI.SLIDERS.kContrast);
     }
+    //if (checkDistorcion())
+    //{
+    //
+    //}
   }
 
   bool checkFocusDistance()
